63fc4bf94f1d5868d61f97fe0389603f624a8628
Laurent Grangeau <laurent.grangeau@gmail.com>
2017-05-04 01:24:12 +0200

    Optimize

--- ethminer/docker-entrypoint.sh
+++ ethminer/docker-entrypoint.sh
@@ -2,4 +2,4 @@
 set -e
 
 ldconfig /usr/lib/ /opt/amdgpu-pro/lib/x86_64-linux-gnu/
-ethminer -C -G --farm-recheck 200 -F http://$PROXY_IP:$PROXY_PORT/$RIG
+ethminer -G --farm-recheck 400 -F http://$PROXY_IP:$PROXY_PORT/$RIG --cl-local-work 256 --cl-global-work 16384
